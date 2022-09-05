9986c5526e3cd3ec74387967526cb9433608ca65
sandrasebastian <85393618+sandrasebastian@users.noreply.github.com>
2022-09-06 00:39:43 +0530

    Add files via upload

---
+++ JAVA/switch/Size2.java
@@ -0,0 +1,27 @@
+package com.ust_switch;
+
+public class Size2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int expression=9;
+		switch(expression) {
+		case 2:
+		System.out.println("Small Size");
+		break;
+		
+		case 3:
+			System.out.println("Large Size");
+			break;
+			
+		//default case 
+		default:
+		
+			System.out.println("Unknown Size");
+			break;
+		
+		}
+
+	}
+
+}
